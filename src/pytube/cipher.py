5cc1438e584e3ed32a04b9b78261926f52c71675
HermanPlay <hermanplay1110@gmail.com>
2023-08-24 21:47:50 +0200

    Fixed cipher regex

--- src/pytube/cipher.py
+++ src/pytube/cipher.py
@@ -264,7 +264,7 @@ def get_throttling_function_name(js: str) -> str:
         # Bpa.length || iha("")) }};
         # In the above case, `iha` is the relevant function name
         r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&.*?\|\|\s*([a-z]+)',
-        r"\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)",
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
     ]
     logger.debug("Finding throttling function name")
     for pattern in function_patterns:
@@ -278,7 +278,7 @@ def get_throttling_function_name(js: str) -> str:
             if idx:
                 idx = idx.strip("[]")
                 array = re.search(
-                    r"var {nfunc}\s*=\s*(\[.+?\]);".format(
+                    r"var {nfunc}\s*=\s*(\[.+?\])".format(
                         nfunc=re.escape(function_match.group(1))
                     ),
                     js,
