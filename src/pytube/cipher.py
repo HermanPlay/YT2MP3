adb3b0da8d7ad61a6ddb76e7c6f5bab8f718348a
HermanPlay <hermanplay1110@gmail.com>
2023-08-24 22:13:11 +0200

    Fix regex

--- src/pytube/cipher.py
+++ src/pytube/cipher.py
@@ -264,7 +264,7 @@ def get_throttling_function_name(js: str) -> str:
         # Bpa.length || iha("")) }};
         # In the above case, `iha` is the relevant function name
         r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&.*?\|\|\s*([a-z]+)',
-        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
     ]
     logger.debug("Finding throttling function name")
     for pattern in function_patterns:
